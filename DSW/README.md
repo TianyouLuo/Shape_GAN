e159ca09ec34b2c13e33e31b4dd2db80b096d04a
TianyouLuo <tianyou@live.unc.edu>
2021-10-04 15:01:11 -0400

    Add initial DSWD codes
    Using MNIST fully connected architecture

---
+++ DSW/README.md
@@ -0,0 +1,112 @@
+## Distributional Sliced Wasserstein distance
+This is a pytorch implementation of distributional sliced Wasserstein which is a sliced optimal transport distance between two probability measures. Details of the model architecture and experimental results can be found in [our following paper](https://arxiv.org/pdf/2002.07367.pdf).
+````
+@inproceedings{
+nguyen2021distributional,
+title={Distributional Sliced-Wasserstein and Applications to Generative Modeling},
+author={Khai Nguyen and Nhat Ho and Tung Pham and Hung Bui},
+booktitle={International Conference on Learning Representations},
+year={2021},
+url={https://openreview.net/forum?id=QYjO70ACDK}
+}
+````
+Please CITE our paper whenever this repository is used to help produce published results or incorporated into other software.
+### Installation
+
+* python>=3.6
+* pytorch>=1.4
+* torchvision
+* numpy
+* tqdm
+* POT
+* scikit-image
+
+````
+pip install torch==1.4.0 torchvision==0.5.0 numpy tqdm POT scikit-image
+````
+### Train on MNIST
+``` 
+python mnist.py \
+    --datadir='./' \
+    --outdir='./result' \
+    --batch-size=512 \
+    --seed=16 \
+    --p=2 \
+    --lr=0.0005 \
+    --dataset='MNIST'
+    --model-type='DSWD'\
+    --latent-size=32 \ 
+```
+```
+model-type in (SWD|MSWD|DSWD|GSWD|DGSWD|JSWD|JMSWD|JDSWD|JGSWD|JDGSWD|MGSWNN|JMGSWNN|MGSWD|JMGSWD)
+```
+Options for Sliced distances (number of projections used to approximate the distances)
+````
+--num-projection=1000
+````
+
+Options for Max Sliced-Wasserstein distance and Distributional distances (number of gradient steps for find the max slice or the optimal push-forward function):
+
+```
+--niter=10
+```
+Options for Distributional Sliced-Wasserstein Distance and Distributional Generalized Sliced-Wasserstein Distance (regularization strength)
+
+````
+--lam=10
+````
+Options for Generalized Wasserstein Distance (using circular function for Generalized Radon Transform)
+
+````
+--r=1000;\
+--g='circular'
+````
+### Train on CELEBA and CIFAR10 and LSUN
+``` 
+python main.py \
+    --datadir='./' \
+    --outdir='./result' \
+    --batch-size=512 \
+    --seed=16 \
+    --p=2 \
+    --lr=0.0005 \
+    --model-type='DSWD'\
+    --dataset='CELEBA'
+    --latent-size=100 \ 
+```
+```
+model-type in (SWD|MSWD|DSWD|GSWD|DGSWD)
+```
+Options for Sliced distances (number of projections used to approximate the distances)
+````
+--num-projection=1000
+````
+Options for Max Sliced-Wasserstein distance and Distributional distances (number of gradient steps for find the max slice or the optimal push-forward function):
+
+```
+--niter=1
+```
+Options for Distributional Sliced-Wasserstein Distance and Distributional Generalized Sliced-Wasserstein Distance (regularization strength)
+
+````
+--lam=1
+````
+Options for Generalized Wasserstein Distance (using circular function for Generalized Radon Transform)
+
+````
+--r=1000;\
+--g='circular'
+````
+### Evaluation
+Please use https://github.com/bioinf-jku/TTUR for evaluating the trained generative models.
+
+### Some generated images
+##### MNIST generated images
+![MNIST](images/MNIST.png) 
+##### CELEBA generated images
+![MNIST](images/CelebA.png) 
+##### LSUN generated images
+![MNIST](images/LSUN.png) 
+
+### Acknowledgement
+Our code uses the implementation of Max-SW, GSW, Max-GSW-NN from https://github.com/kimiandj/gsw.
---
+++ DSW/run_yalin_debug.py
@@ -0,0 +1,339 @@
+from __future__ import print_function
+
+import argparse
+import os
+
+import numpy as np
+import glob
+import torch
+import torchvision.datasets as datasets
+from experiments import reconstruct, sampling, sampling_eps
+from gsw import GSW
+import pandas as pd
+# torch.backends.cudnn.enabled = False
+from gswnn import GSW_NN
+from ShapeAutoencoder import ShapeAutoencoder
+from torch import optim
+from torchvision import transforms
+from tqdm import tqdm
+from TransformNet import TransformNet
+from utils import circular_function, compute_true_Wasserstein, save_dmodel, sliced_wasserstein_distance
+
+
+# train args
+parser = argparse.ArgumentParser(description="Disributional Sliced Wasserstein Autoencoder")
+parser.add_argument("--datadir", default="./", help="path to dataset")
+parser.add_argument("--outdir", default="./result", help="directory to output images")
+parser.add_argument(
+    "--batch-size", type=int, default=512, metavar="N", help="input batch size for training (default: 512)"
+)
+parser.add_argument(
+    "--epochs", type=int, default=200, metavar="N", help="number of epochs to train (default: 200)"
+)
+parser.add_argument("--lr", type=float, default=0.0005, metavar="LR", help="learning rate (default: 0.0005)")
+parser.add_argument(
+    "--num-workers",
+    type=int,
+    default=16,
+    metavar="N",
+    help="number of dataloader workers if device is CPU (default: 16)",
+)
+parser.add_argument("--seed", type=int, default=16, metavar="S", help="random seed (default: 16)")
+parser.add_argument("--g", type=str, default="circular", help="g")
+parser.add_argument("--num-projection", type=int, default=1000, help="number projection")
+parser.add_argument("--lam", type=float, default=1, help="Regularization strength")
+parser.add_argument("--p", type=int, default=2, help="Norm p")
+parser.add_argument("--niter", type=int, default=10, help="number of iterations")
+parser.add_argument("--r", type=float, default=1000, help="R")
+parser.add_argument("--kappa", type=float, default=50, help="R")
+parser.add_argument("--k", type=int, default=10, help="R")
+parser.add_argument("--e", type=float, default=1000, help="R")
+parser.add_argument("--latent-size", type=int, default=32, help="Latent size")
+parser.add_argument("--hsize", type=int, default=100, help="h size")
+parser.add_argument("--dataset", type=str, default="MNIST", help="(MNIST|FMNIST)")
+parser.add_argument(
+    "--model-type", type=str, required=True, help="(SWD|MSWD|DSWD|GSWD|DGSWD|JSWD|JMSWD|JDSWD|JGSWD|JDGSWD)"
+)
+args = parser.parse_args()
+
+torch.random.manual_seed(2021)
+#if args.g == "circular":
+#    g_function = circular_function
+model_type = 'DSWD'
+latent_size = 100
+num_projection = 10
+dataset = 'Yalin'
+image_size = 100 * 150
+num_chanel = 1
+outdir = '/pine/scr/t/i/tianyou/Yalin_GAN/DSW/result/'
+niter = 10
+lam = 10
+batch_size = 64
+epochs = 100
+lr = 0.0001
+p = 2
+assert model_type in [
+    "MGSWNN",
+    "JMGSWNN",
+    "SWD",
+    "MSWD",
+    "MGSWD",
+    "DSWD",
+    "GSWD",
+    "DGSWD",
+    "JSWD",
+    "JMSWD",
+    "JMGSWD",
+    "JDSWD",
+    "JGSWD",
+    "JDGSWD",
+    "DGSWNN",
+    "JDGSWNN",
+    "GSWNN",
+    "JGSWNN",
+]
+if model_type == "SWD" or model_type == "JSWD":
+    model_dir = os.path.join(outdir, model_type + "_n" + str(num_projection))
+elif model_type == "GSWD" or model_type == "JGSWD":
+    model_dir = os.path.join(args.outdir, model_type + "_n" + str(num_projection) + "_" + args.g + str(args.r))
+elif model_type == "DSWD" or model_type == "JDSWD":
+    model_dir = os.path.join(
+        outdir, model_type + "_iter" + str(niter) + "_n" + str(num_projection) + "_lam" + str(lam)
+    )
+elif model_type == "DGSWD" or model_type == "JDGSWD":
+    model_dir = os.path.join(
+        args.outdir,
+        model_type
+        + "_iter"
+        + str(args.niter)
+        + "_n"
+        + str(num_projection)
+        + "_lam"
+        + str(args.lam)
+        + "_"
+        + args.g
+        + str(args.r),
+    )
+elif model_type == "MSWD" or model_type == "JMSWD":
+    model_dir = os.path.join(args.outdir, model_type)
+elif model_type == "MGSWNN" or model_type == "JMGSWNN":
+    model_dir = os.path.join(args.outdir, model_type + "_size" + str(args.hsize))
+elif model_type == "MGSWD" or model_type == "JMGSWD":
+    model_dir = os.path.join(args.outdir, model_type + "_" + args.g)
+if not (os.path.isdir(args.datadir)):
+    os.makedirs(args.datadir)
+if not (os.path.isdir(args.outdir)):
+    os.makedirs(args.outdir)
+if not (os.path.isdir(args.outdir)):
+    os.makedirs(args.outdir)
+if not (os.path.isdir(model_dir)):
+    os.makedirs(model_dir)
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+print(
+    "batch size {}\nepochs {}\nAdam lr {} \n using device {}\n".format(
+        batch_size, epochs, lr, device.type
+    )
+)
+# build training set data loaders
+if(dataset=='Yalin'):
+    datadir='/pine/scr/t/i/tianyou/Yalin_GAN/data/ADNIHippoCSV/'
+    files = glob.glob(datadir+'/*.csv')
+    
+    train_data = np.zeros((len(files),100,150), dtype=np.float32)
+    
+    # checking all the csv files in the 
+    # specified path
+    for l in range(len(files)):
+        # reading content of csv file
+        # content.append(filename)
+        if l % 50 == 0:
+            print(len(files) - l)
+        filename = files[l]
+        df = pd.read_csv(filename, names=['f1','f2','f3','f4','f5','f6','f7'],sep=' ')
+        df_sel = df.iloc[0:15000,0].to_numpy(dtype = np.float32)  ## only work with one modality for now
+        df_reshape = df_sel.reshape((100,150), order = 'F')
+        train_data[l,:,:] = df_reshape
+    
+    pmax = np.max(train_data)
+    pmin = np.min(train_data)
+    train_norm = train_data / pmax
+    X = torch.tensor(train_norm, dtype=torch.float32).to(device)
+    train_loader = torch.utils.data.DataLoader(X, batch_size=batch_size, shuffle=True)
+    model = ShapeAutoencoder(image_size=100 * 150, latent_size=latent_size, hidden_size=100, device=device).to(device)
+
+if model_type == "DSWD" or model_type == "DGSWD":
+    transform_net = TransformNet(100 * 150).to(device)
+    op_trannet = optim.Adam(transform_net.parameters(), lr=lr, betas=(0.5, 0.999))
+    # op_trannet = optim.Adam(transform_net.parameters(), lr=1e-4)
+    # train_net(28 * 28, 1000, transform_net, op_trannet)
+elif model_type == "JDSWD" or model_type == "JDSWD2" or model_type == "JDGSWD":
+    transform_net = TransformNet(args.latent_size + 28 * 28).to(device)
+    op_trannet = optim.Adam(transform_net.parameters(), lr=args.lr, betas=(0.5, 0.999))
+    # train_net(args.latent_size + 28 * 28, 1000, transform_net, op_trannet)
+if model_type == "MGSWNN":
+    gsw = GSW_NN(din=28 * 28, nofprojections=1, model_depth=3, num_filters=args.hsize, use_cuda=True)
+if model_type == "GSWNN" or model_type == "DGSWNN":
+    gsw = GSW_NN(din=28 * 28, nofprojections=num_projection, model_depth=3, num_filters=args.hsize, use_cuda=True)
+if model_type == "JMGSWNN":
+    gsw = GSW_NN(din=28 * 28 + 32, nofprojections=1, model_depth=3, num_filters=args.hsize, use_cuda=True)
+if model_type == "MSWD" or model_type == "JMSWD":
+    gsw = GSW()
+if model_type == "MGSWD":
+    theta = torch.randn((1, 784), device=device, requires_grad=True)
+    theta.data = theta.data / torch.sqrt(torch.sum(theta.data ** 2, dim=1))
+    opt_theta = optim.Adam(transform_net.parameters(), lr=args.lr, betas=(0.5, 0.999))
+if model_type == "JMGSWD":
+    theta = torch.randn((1, 784 + 32), device=device, requires_grad=True)
+    theta.data = theta.data / torch.sqrt(torch.sum(theta.data ** 2, dim=1))
+    opt_theta = torch.optim.Adam(transform_net.parameters(), lr=args.lr, betas=(0.5, 0.999))
+optimizer = optim.Adam(model.parameters(), lr=lr, betas=(0.5, 0.999))
+fixednoise = torch.randn((128, latent_size)).to(device)
+ite = 0
+wd_list = []
+swd_list = []
+for epoch in range(epochs):
+    total_loss = 0.0
+    for batch_idx, data in tqdm(enumerate(train_loader, start=0)):
+        if model_type == "SWD":
+            loss = model.compute_loss_SWD(data, torch.randn, num_projection, p=p)
+        elif model_type == "GSWD":
+            loss = model.compute_loss_GSWD(data, torch.randn, g_function, args.r, num_projection, p=2)
+        elif model_type == "GSWNN":
+            loss = model.compute_loss_GSWNN(data, torch.randn, gsw, p=2)
+        elif model_type == "DGSWNN":
+            loss = model.compute_loss_DGSWNN(data, torch.randn, gsw, args.niter, args.lam, args.lr, p=2)
+        elif model_type == "MGSWNN":
+            loss = model.compute_loss_MGSWNN(data, torch.randn, gsw, args.niter, p=args.p)
+        elif model_type == "JMGSWNN":
+            loss = model.compute_loss_JMGSWNN(data, torch.randn, gsw, args.niter, p=args.p)
+        elif model_type == "MSWD":
+            loss = model.compute_loss_MSWD(data, torch.randn, gsw, args.niter)
+        elif model_type == "MGSWD":
+            loss = model.compute_loss_MGSWD(
+                data, torch.randn, theta, opt_theta, g_function, args.r, args.lr2, p=args.p, max_iter=args.niter
+            )
+        elif model_type == "DSWD":
+            loss = model.compute_lossDSWD(
+                data,
+                torch.randn,
+                num_projection,
+                transform_net,
+                op_trannet,
+                p=p,
+                max_iter=niter,
+                lam=lam,
+            )
+        elif model_type == "DGSWD":
+            loss = model.compute_lossDGSWD(
+                data,
+                torch.randn,
+                num_projection,
+                transform_net,
+                op_trannet,
+                g_function,
+                r=args.r,
+                p=args.p,
+                max_iter=args.niter,
+                lam=args.lam,
+            )
+        elif model_type == "JSWD":
+            loss = model.compute_loss_JSWD(data, torch.randn, num_projection, p=args.p)
+        elif model_type == "JGSWD":
+            loss = model.compute_loss_JGSWD(data, torch.randn, g_function, args.r, num_projection, p=args.p)
+        elif model_type == "JDSWD":
+            loss = model.compute_lossJDSWD(
+                data,
+                torch.randn,
+                num_projection,
+                transform_net,
+                op_trannet,
+                p=args.p,
+                max_iter=args.niter,
+                lam=args.lam,
+            )
+        elif model_type == "JDGSWD":
+            loss = model.compute_lossJDGSWD(
+                data,
+                torch.randn,
+                num_projection,
+                transform_net,
+                op_trannet,
+                g_function,
+                r=args.r,
+                p=args.p,
+                max_iter=args.niter,
+                lam=args.lam,
+            )
+        elif model_type == "JMSWD":
+            loss = model.compute_loss_JMSWD(data, torch.randn, gsw, args.niter)
+        elif model_type == "JMGSWD":
+            loss = model.compute_loss_JMGSWD(
+                data, torch.randn, theta, opt_theta, g_function, args.r, p=args.p, max_iter=args.niter
+            )
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+#        if ite % 100 == 0:
+#            model.eval()
+#            for _, (input, y) in enumerate(test_loader, start=0):
+#                fixednoise_wd = torch.randn((10000, latent_size)).to(device)
+#                data = input.to(device)
+#                data = data.view(data.shape[0], -1)
+#                fake = model.decoder(fixednoise_wd)
+#                wd_list.append(compute_true_Wasserstein(data.to("cpu"), fake.to("cpu")))
+#                swd_list.append(sliced_wasserstein_distance(data, fake, 10000).item())
+#                print("Iter:" + str(ite) + " WD: " + str(wd_list[-1]))
+#                np.savetxt(model_dir + "/wd.csv", wd_list, delimiter=",")
+#                print("Iter:" + str(ite) + " SWD: " + str(swd_list[-1]))
+#                np.savetxt(model_dir + "/swd.csv", swd_list, delimiter=",")
+#                break
+#            model.train()
+        ite = ite + 1
+    total_loss /= batch_idx + 1
+    print("Epoch: " + str(epoch) + " Loss: " + str(total_loss))
+    
+#    if epoch % 1 == 0:
+#        model.eval()
+#        sampling(
+#            model_dir + "/sample_epoch_" + str(epoch) + ".png",
+#            fixednoise,
+#            model.decoder,
+#            64,
+#            image_size,
+#            num_chanel,
+#        )
+#        if model_type[0] == "J":
+#            for _, (input, y) in enumerate(test_loader2, start=0):
+#                input = input.to(device)
+#                input = input.view(-1, image_size ** 2)
+#                reconstruct(
+#                    model_dir + "/reconstruction_epoch_" + str(epoch) + ".png",
+#                    input,
+#                    model.encoder,
+#                    model.decoder,
+#                    image_size,
+#                    num_chanel,
+#                    device,
+#                )
+#                break
+#        model.train()
+    save_dmodel(model, optimizer, None, None, None, None, epoch, model_dir)
+    if (epoch % 10 == 0):
+        model.eval()
+        samp = model.decoder(fixednoise)
+        samp_num = samp.detach().to('cpu').numpy()
+        samp_pd = pd.DataFrame(samp_num)
+        samp_pd.to_csv(model_dir+"/samp_epoch"+str(epoch)+".csv", index = False)
+        model.train()
+        
+#    if epoch == args.epochs - 1:
+#        model.eval()
+#        sampling_eps(
+#            model_dir + "/sample_epoch_" + str(epoch), fixednoise, model.decoder, 64, image_size, num_chanel
+#        )
+#        model.train()
+
+
